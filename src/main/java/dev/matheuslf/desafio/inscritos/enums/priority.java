f0130dceafa49f15c473f79f083c12eadff222e9
diegometring <diegometringteixeira@gmail.com>
2025-10-20 08:16:48 -0300

    create enums

---
+++ src/main/java/dev/matheuslf/desafio/inscritos/enums/priority.java
@@ -0,0 +1,7 @@
+package dev.matheuslf.desafio.inscritos.enums;
+
+public enum priority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ src/main/java/dev/matheuslf/desafio/inscritos/enums/statusTask.java
@@ -0,0 +1,7 @@
+package dev.matheuslf.desafio.inscritos.enums;
+
+public enum statusTask {
+        TODO,
+        DOING,
+        DONE
+}
